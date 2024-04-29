8499aa285f4f0cc96a81278a602d6c7846bfcbfc
Sigmund Tzeng <jawatech@gmail.com>
2024-04-29 11:18:25 +0800

    Update main.py with generate_markdown_template()

--- main.py
+++ main.py
@@ -304,9 +304,9 @@ class _OverloadTasks:
         _project = f"# arxiv-daily\n"
         _pin = f" Automated deployment @ {self.update_time} Asia/Taipei\n"
         _tos = "> Welcome to contribute! Add your topics and keywords in " \
-               "[`topic.yml`](https://github.com/weiningwei/arxiv-daily/blob/main/database/topic.yml).\n"
+               "[`topic.yml`](https://github.com/jawatech/arxiv-daily-in-place/blob/main/database/topic.yml).\n"
         _tos += "> You can also view historical data through the " \
-                "[storage](https://github.com/weiningwei/arxiv-daily/blob/main/database/storage).\n"
+                "[storage](https://github.com/jawatech/arxiv-daily-in-place/blob/main/database/storage).\n"
 
         _form = _project + _pin + _tos + content
 

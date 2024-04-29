927243f2715f893c43f6250e96abccc9496b7e18
Sigmund Tzeng <jawatech@gmail.com>
2024-04-29 08:09:18 +0800

    Create util.py for translating english into mandarin

---
+++ util.py
@@ -0,0 +1,49 @@
+import html
+import os
+import random
+import re
+import time
+from collections import defaultdict
+from datetime import datetime
+from urllib import parse
+
+import requests
+
+GOOGLE_TRANSLATE_URL = 'https://translate.google.com/m?q=%s&tl=%s&sl=%s'
+HEADERS = {
+    'User-Agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0',
+    'Connection': 'close'
+}
+
+
+def get_week_dates(root):
+    week_dates = defaultdict(list)
+    for file in os.scandir(root):
+        date = file.name.split('.')[0]
+        week = datetime.strptime(date, '%Y-%m-%d').strftime('%Y-W%W')
+        week_dates[week].append(date)
+    return week_dates
+
+
+def translate(text, to_language="zh-CN", text_language="en"):
+    time.sleep(random.random() + 1)
+    text = parse.quote(text)
+    url = GOOGLE_TRANSLATE_URL % (text, to_language, text_language)
+    response = None
+    for i in range(5):
+        try:
+            response = requests.get(url, timeout=2)
+            break
+        except Exception:
+            time.sleep(1)
+    if response is None:
+        return ""
+
+    data = response.text
+    expr = r'(?s)class="(?:t0|result-container)">(.*?)<'
+    result = re.findall(expr, data)
+    if len(result) == 0:
+        return ""
+
+    return html.unescape(result[0])
